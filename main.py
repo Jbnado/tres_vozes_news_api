76fbcf2026bf9ff5de7929ad2b73c6e53ee1b8f7
Jbnado <bernardojoao9@gmail.com>
2023-06-10 13:12:57 -0300

    ci: flask port and run commands

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_cors import CORS
 from db import config_db
@@ -5,6 +7,8 @@ from user.user_controller import user_blueprint
 from topic.topic_controller import topic_blueprint
 from news.news_controller import news_blueprint
 from liked.liked_controller import liked_blueprint
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -16,4 +20,5 @@ app.register_blueprint(news_blueprint, url_prefix="/news")
 app.register_blueprint(liked_blueprint, url_prefix="/liked")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
